0902f4bb5c89386d7c3cfc77607221ab945ccbfb
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2022-08-31 13:47:03 +0100

    CircleCI coverage artifacts test commit

--- modules/types-lib/jest.config.js
+++ modules/types-lib/jest.config.js
@@ -1,10 +1,14 @@
-'use strict'
+"use strict"
+
+const { name } = require("./package.json");
+let packageName = name.replace("@mojaloop", "");
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: false,
-  collectCoverageFrom: ['./src/**/*.ts'],
-  coverageReporters: ['json', 'lcov'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**/*.ts"],
+  coverageReporters: ["json", "lcov"],
+  coverageDirectory: `../../coverage/${packageName}/`,
   clearMocks: true
 }
